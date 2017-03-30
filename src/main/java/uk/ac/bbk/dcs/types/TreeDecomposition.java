bc353581bbf8925b2b140d42f61b528a5cabb511
Salvatore Rapisarda <salvatorer@halfordsmedia.co.uk>
2017-03-30 17:02:48 +0100

    added Hyper-Tree Bag. class

---
+++ src/main/java/uk/ac/bbk/dcs/types/TreeDecomposition.java
@@ -0,0 +1,18 @@
+package uk.ac.bbk.dcs.types;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+
+/**
+ * Created by :
+ *          Salvatore Rapisarda
+ *          Stanislav Kikot
+ *
+ * on 30/03/2017.
+ */
+public class TreeDecomposition {
+    Bag root;
+    ImmutableMap< Bag, ImmutableSet<Bag>> successor;
+
+    // todo: implement this;
+}
